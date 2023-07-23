161a71d7b5fe44f029afa5f1c60c8a2a06d74171
ColorFilter <johan@romantech.net>
2023-07-24 02:06:44 +0900

    📦 chore(.eslintrc.cjs): add ESLint configuration file to project for linting rules and plugins 📝 docs(.gitignore): add .gitignore file to project to exclude certain files and directories from version control 📝 docs(.prettierrc): add .prettierrc file to project for Prettier configuration 📝 docs(README.md): update README.md file with project information and instructions 📝 docs(index.html): add index.html file to project with basic HTML structure 📝 docs(package.json): add package.json file to project with dependencies and scripts 📝 docs(public/vite.svg): add vite.svg file to project for favicon 📝 docs(src/App.tsx): add App component to project with basic structure 📝 docs(src/index.css): add index.css file to project with global styles 📝 docs(src/main.tsx): add main.tsx file to project with ReactDOM rendering the App component 📝 docs(src/vite-env.d.ts): add vite-env.d.ts file to project for Vite environment types 📝 docs(tsconfig.json): add tsconfig.json file to project with compiler options and paths 📝 docs(tsconfig.node.json): add tsconfig.node.json file to project for Node.js specific compiler options 📝 docs(vite.config.ts): add vite.config.ts file to project with Vite configuration options

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Prettier 가 잡은 내용은 'warn' 으로 취급하도록 설정(ESLint - Prettier 충돌 방지) ▼
+    // { endOfLine: 'auto' }는 엔드라인 시퀀스 자동 변경 ▼
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    'no-param-reassign': ['error', { props: false }],
+    'prefer-const': 1,
+    'no-plusplus': 0,
+    'vars-on-top': 0,
+    'no-underscore-dangle': 0, // var _foo;
+    'comma-dangle': 0,
+    'func-names': 0, // setTimeout(function () {}, 0);
+    'prefer-arrow-callback': 0, // setTimeout(function () {}, 0);
+    'prefer-template': 0,
+    'no-nested-ternary': 0,
+    'max-classes-per-file': 0,
+    'no-restricted-syntax': [0, 'ForOfStatement'],
+    'consistent-return': 1,
+    'no-unused-vars': 0,
+    'no-unused-expressions': 1,
+    'react/prop-types': 0,
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      { additionalHooks: '(useRecoilCallback|useRecoilTransaction_UNSTABLE)' },
+    ],
+    '@typescript-eslint/no-unused-vars': 1,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>Hello World</div>;
+}
+
+export default App;
