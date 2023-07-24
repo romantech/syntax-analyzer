c4e81565c8532e7eed752330a24b6a50f27c466f
ColorFilter <johan@romantech.net>
2023-07-24 21:22:39 +0900

    🚀 chore(package.json): add react-router-dom as a dependency to enable routing in the application 🔧 refactor(main.tsx): replace App component with a router configuration using react-router-dom to enable routing functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
 
 ReactDOM.createRoot(rootElement!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 );
