f5db5256b80ae36897e655a0e5e1bd0a178593c4
ColorFilter <johan@romantech.net>
2023-07-24 19:18:18 +0900

    🔧 chore(.eslintrc.cjs): update ESLint configuration to include Node.js environment and reorganize plugin order for better readability 📦 chore(package.json): add new dependencies for Chakra UI, Emotion, and Framer Motion, and update @types/node dependency version ✨ feat(src/main.tsx): wrap the App component with ChakraProvider from Chakra UI for better styling and theming support 🔧 chore(vite.config.ts): add server configuration to specify the port as 3000 for development environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, // Node.js 환경의 전역 변수 등록
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react/recommended',
+    'eslint:recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react/react-in-jsx-scope': 'off',
