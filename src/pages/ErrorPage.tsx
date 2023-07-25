5a206e63bbbed4df002611344423147f162c458c
ColorFilter <johan@romantech.net>
2023-07-25 12:55:10 +0900

    🎨 style(ErrorPage.tsx): add ScaleFade animation to improve user experience when rendering the ErrorPage component

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,14 +1,16 @@
-import { Heading, Text, VStack } from '@chakra-ui/react';
+import { Heading, ScaleFade, Text, VStack } from '@chakra-ui/react';
 
 export default function ErrorPage() {
   return (
     <VStack justify="center" align="center" minH="90%">
-      <Heading as="h1" size="4xl">
-        Ooops!
-      </Heading>
-      <Text fontSize="xl" textTransform="uppercase">
-        404 | page not found
-      </Text>
+      <ScaleFade initialScale={0.1} in={true}>
+        <Heading as="h1" size="4xl">
+          Ooops!
+        </Heading>
+        <Text fontSize="xl" textTransform="uppercase">
+          404 | page not found
+        </Text>
+      </ScaleFade>
     </VStack>
   );
 }
