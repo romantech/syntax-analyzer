197fb8d7de1f81cf887ea4c879a8e068efd836a3
ColorFilter <johan@romantech.net>
2023-07-25 09:49:36 +0900

    🎨 style(ErrorPage.tsx): adjust min height of VStack to 90% to improve page layout 🐛 fix(ErrorPage.tsx): remove unnecessary prop 'noOfLines' from Heading component to fix warning

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,8 +2,8 @@ import { Heading, Text, VStack } from '@chakra-ui/react';
 
 export default function ErrorPage() {
   return (
-    <VStack justify="center" align="center" minH="full">
-      <Heading as="h1" size="4xl" noOfLines={1}>
+    <VStack justify="center" align="center" minH="90%">
+      <Heading as="h1" size="4xl">
         Ooops!
       </Heading>
       <Text fontSize="xl" textTransform="uppercase">
