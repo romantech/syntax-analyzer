44b31e3177ab97b4d9178ff28990f9fb465bd29d
ColorFilter <johan@romantech.net>
2023-07-25 12:58:03 +0900

    🎨 style(ErrorPage.tsx): add gap between elements in VStack to improve spacing 🐛 fix(ErrorPage.tsx): add missing closing ScaleFade tag for error message to fix JSX syntax error

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,11 +2,13 @@ import { Heading, ScaleFade, Text, VStack } from '@chakra-ui/react';
 
 export default function ErrorPage() {
   return (
-    <VStack justify="center" align="center" minH="90%">
+    <VStack justify="center" align="center" minH="90%" gap={4}>
       <ScaleFade initialScale={0.1} in={true}>
         <Heading as="h1" size="4xl">
           Ooops!
         </Heading>
+      </ScaleFade>
+      <ScaleFade initialScale={0.1} in={true}>
         <Text fontSize="xl" textTransform="uppercase">
           404 | page not found
         </Text>
