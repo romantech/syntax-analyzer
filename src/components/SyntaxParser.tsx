19e2d250c2eb34c92f04b9861e88448d20f9cb03
ColorFilter <johan@romantech.net>
2023-07-28 19:08:05 +0900

    🔨 refactor(SyntaxParser.tsx): Remove unused imports and delete RootSegment component 🔨 refactor(index.ts): Remove export of RootSegment component 🔧 chore(tsconfig.json): Add a comment about avoiding uppercase letters in folder names when using aliases

--- src/components/SyntaxParser.tsx
+++ src/components/SyntaxParser.tsx
@@ -2,16 +2,27 @@ import { SlideFade } from '@chakra-ui/react';
 import { useRef } from 'react';
 import '@/styles/constituent.scss';
 import { useCalculateNestingLevel } from '@/hooks';
-import { RootSegment, Sentence } from '@/components/syntax-parser';
+import { SegmentList, Sentence, TokenList } from '@/components/syntax-parser';
+import { useAtomValue } from 'jotai/index';
+import { currentSegmentAtom } from '@/store/segmentHistoryStore';
+import { currentSentenceAtom } from '@/store/analysisStore';
 
 export default function SyntaxParser() {
   const sentenceRef = useRef<HTMLParagraphElement>(null);
   const isNestingLevelCalculated = useCalculateNestingLevel(sentenceRef);
 
+  const segment = useAtomValue(currentSegmentAtom);
+  const sentence = useAtomValue(currentSentenceAtom);
+  const hasAnalysisData = segment && sentence;
+  if (!hasAnalysisData) return null;
+
   return (
     <SlideFade in={isNestingLevelCalculated} offsetY={100}>
       <Sentence ref={sentenceRef}>
-        <RootSegment />
+        <SegmentList
+          segment={segment}
+          tokenElements={TokenList({ sentence })}
+        />
       </Sentence>
     </SlideFade>
   );
