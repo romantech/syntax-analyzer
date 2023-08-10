296e9b7e5fbcf12a0f7acc6a61b2cb9eed7cdd50
ColorFilter <johan@romantech.net>
2023-08-10 15:51:00 +0900

    🐛 fix(token-list.tsx): change spaceAfter and spaceBefore variables from numbers to '3px' to fix spacing issue in TokenList component

--- src/features/syntax-editor/components/syntax-parser/token-list.tsx
+++ src/features/syntax-editor/components/syntax-parser/token-list.tsx
@@ -9,8 +9,8 @@ export default function TokenList({ sentence }: TokensProps) {
   return sentence.map((token, i) => {
     const isNextTokenPunctuation = isPunctuation(sentence[i + 1]);
     const isCurrentTokenPunctuation = isPunctuation(token);
-    const spaceAfter = isNextTokenPunctuation ? 0 : 1;
-    const spaceBefore = isCurrentTokenPunctuation ? 0 : 1;
+    const spaceAfter = isNextTokenPunctuation ? 0 : '3px';
+    const spaceBefore = isCurrentTokenPunctuation ? 0 : '3px';
     return (
       <Token
         token={token}
