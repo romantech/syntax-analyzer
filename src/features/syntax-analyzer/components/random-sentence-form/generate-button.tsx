bf9689e49c790f6e80292e55c586351fec1968be
ColorFilter <johan@romantech.net>
2023-08-16 03:11:30 +0900

    🐛 fix(generate-button.tsx): fix tooltip label to display a message when count is 0 and disable the button ✨ feat(generate-button.tsx): add tooltip to provide information when count is 0 and button is disabled 🎨 style(generate-button.tsx): adjust button width to 190 to match tooltip width 🎨 style(generate-button.tsx): adjust skeleton width to 190 to match button width

--- src/features/syntax-analyzer/components/random-sentence-form/generate-button.tsx
+++ src/features/syntax-analyzer/components/random-sentence-form/generate-button.tsx
@@ -7,6 +7,7 @@ import {
   ButtonProps,
   Skeleton,
   Text,
+  Tooltip,
   useColorModeValue,
 } from '@chakra-ui/react';
 import { RiAiGenerate } from 'react-icons/ri';
@@ -20,30 +21,39 @@ export default function GenerateButton({
     select: (data) => data.random_sentence,
     suspense: true,
   });
+  const countColor = useColorModeValue('gray.400', 'gray.500');
+  const hasCount = count > 0;
 
   return (
-    <Button
-      w={191}
-      onClick={onClick}
-      leftIcon={<RiAiGenerate />}
-      textTransform="uppercase"
-      isLoading={isLoading}
-      isDisabled={count === 0}
-      {...buttonProps}
+    <Tooltip
+      isDisabled={hasCount}
+      label={'사용량을 모두 소진했어요\n내일 다시 시도해주세요'}
+      placement="top"
+      whiteSpace="pre-line"
     >
-      <Text as="span">generate</Text>
-      <Text
-        pl={1}
-        as="span"
-        color={useColorModeValue('gray.400', 'gray.500')}
-        fontSize="sm"
-      >{`(${count}/${MAX_SENTENCE_COUNT})`}</Text>
-    </Button>
+      <Button
+        w={190}
+        onClick={onClick}
+        leftIcon={<RiAiGenerate />}
+        textTransform="uppercase"
+        isLoading={isLoading}
+        isDisabled={!hasCount}
+        {...buttonProps}
+      >
+        <Text as="span">generate</Text>
+        <Text
+          pl={1}
+          as="span"
+          color={countColor}
+          fontSize="sm"
+        >{`(${count}/${MAX_SENTENCE_COUNT})`}</Text>
+      </Button>
+    </Tooltip>
   );
 }
 
 const GenerateButtonSkeleton = () => {
-  return <Skeleton w={191} h={39} borderRadius="md" />;
+  return <Skeleton w={190} h={39} borderRadius="md" />;
 };
 
 GenerateButton.Skeleton = GenerateButtonSkeleton;
