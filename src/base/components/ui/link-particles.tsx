99a770958a417c4dc9799cd8a5c2691f40d436fc
ColorFilter <johan@romantech.net>
2023-08-19 06:53:29 +0900

    🔧 chore(link-particles.tsx): refactor LinkParticles component to use Chakra UI's useColorMode hook for dynamic color selection ✨ feat(link-particles.tsx): add color customization to particles and line_linked properties in options object to match the color mode

--- src/base/components/ui/link-particles.tsx
+++ src/base/components/ui/link-particles.tsx
@@ -1,17 +1,22 @@
 import { loadLinksPreset } from 'tsparticles-preset-links';
 import Particles, { IParticlesProps } from 'react-tsparticles';
-
-const options: IParticlesProps['options'] = {
-  preset: 'links',
-  fullScreen: { enable: false, zIndex: -1 },
-  background: { color: 'transparent' },
-  particles: {
-    opacity: { value: 0.3 },
-    line_linked: { opacity: 0.3 },
-  },
-};
+import { useColorMode } from '@chakra-ui/react';
 
 export default function LinkParticles(particleProps: IParticlesProps) {
+  const isLightMode = useColorMode().colorMode === 'light';
+  const color = isLightMode ? '#5b5b5b' : '#b0b0b0';
+
+  const options: IParticlesProps['options'] = {
+    preset: 'links',
+    fullScreen: { enable: false, zIndex: -1 },
+    background: { color: 'transparent' },
+    particles: {
+      color: { value: color },
+      opacity: { value: 0.3 },
+      line_linked: { opacity: 0.3, color: { value: color } },
+    },
+  };
+
   return (
     <Particles
       style={{ position: 'absolute', top: 0, left: 0 }}
