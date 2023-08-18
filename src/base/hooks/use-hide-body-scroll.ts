970859617c0cc581c3c5a87d29c6c879fc5e274a
ColorFilter <johan@romantech.net>
2023-08-19 06:33:48 +0900

    🐛 fix(use-hide-body-scroll.ts): fix issue with body scroll not being restored properly after unmounting component ✨ feat(use-hide-body-scroll.ts): add useRef to store original body overflow value and restore it on component unmount

--- src/base/hooks/use-hide-body-scroll.ts
+++ src/base/hooks/use-hide-body-scroll.ts
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useHideBodyScroll = () => {
+  const originalOverflow = useRef('');
+
   useEffect(() => {
+    originalOverflow.current = document.body.style.overflow;
     document.body.style.overflow = 'hidden';
 
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = originalOverflow.current;
     };
   }, []);
 };
