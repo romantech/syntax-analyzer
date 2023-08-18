a2951a1385810ff80e1983ed1799b2d4dda7f72d
ColorFilter <johan@romantech.net>
2023-08-19 00:08:31 +0900

    🚀 chore(package.json): add react-tsparticles, tsparticles-engine, and tsparticles-preset-links as dependencies to enable particle effects on the home page 🔧 refactor(layout.tsx): change prop type from StackProps to ContainerProps for better specificity and clarity ✨ feat(home.tsx): add particle effect using react-tsparticles library and configure options and custom initialization function for the particles

--- src/base/components/layout/layout.tsx
+++ src/base/components/layout/layout.tsx
@@ -1,15 +1,15 @@
 import React, { PropsWithChildren } from 'react';
-import { Container, StackProps, VStack } from '@chakra-ui/react';
+import { Container, ContainerProps, VStack } from '@chakra-ui/react';
 import { Header } from '@/base/components';
 
 export function Layout({
   children,
-  ...stackProps
-}: PropsWithChildren<StackProps>) {
+  ...containerProps
+}: PropsWithChildren<ContainerProps>) {
   return (
     <VStack h="100vh" gap={0}>
       <Header />
-      <Container maxW="8xl" flexGrow={1} {...stackProps}>
+      <Container maxW="8xl" flexGrow={1} {...containerProps}>
         {children}
       </Container>
     </VStack>
--- src/features/misc/pages/home.tsx
+++ src/features/misc/pages/home.tsx
@@ -1,8 +1,36 @@
 import { Heading, Stack, Text, VStack } from '@chakra-ui/react';
+import Particles from 'react-tsparticles';
+import { loadLinksPreset } from 'tsparticles-preset-links';
+import { Engine, ISourceOptions } from 'tsparticles-engine';
 
 export default function Home() {
+  const options: ISourceOptions = {
+    preset: 'links',
+    fullScreen: { enable: false, zIndex: -1 },
+    background: { color: 'transparent' },
+    particles: {
+      opacity: { value: 0.3 },
+      line_linked: { opacity: 0.3 },
+    },
+  };
+
+  const customInit = async (engine: Engine) => {
+    await loadLinksPreset(engine);
+  };
+
   return (
-    <VStack gap={8} align="start" minH="90%" justify="center">
+    <VStack
+      position="relative"
+      gap={8}
+      align="start"
+      minH="90%"
+      justify="center"
+    >
+      <Particles
+        style={{ position: 'absolute', top: 0, left: 0 }}
+        options={options}
+        init={customInit}
+      />
       <Stack>
         <Heading
           size="lg"
