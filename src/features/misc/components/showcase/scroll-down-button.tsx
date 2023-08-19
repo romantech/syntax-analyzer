345536be6113a30522d9de2aebc20bbfa26ecac6
ColorFilter <johan@romantech.net>
2023-08-20 04:08:13 +0900

    🔧 fix(scroll-down-button.tsx): change IconButton component to Center component to improve semantics and alignment ✨ feat(scroll-down-button.tsx): add cursor, borderRadius, boxShadow, and _after styles to improve user experience and visual appearance

--- src/features/misc/components/showcase/scroll-down-button.tsx
+++ src/features/misc/components/showcase/scroll-down-button.tsx
@@ -1,26 +1,42 @@
 import { motion } from 'framer-motion';
-import { IconButton, IconButtonProps } from '@chakra-ui/react';
+import { Center, CenterProps } from '@chakra-ui/react';
 import { FaAnglesDown } from 'react-icons/fa6';
 
-const MotionIconButton = motion(IconButton);
+const MotionIconButton = motion(Center);
 
-type ScrollDownButtonProps = Omit<IconButtonProps, 'aria-label'>;
+type ScrollDownButtonProps = Omit<CenterProps, 'aria-label'>;
 
-export default function ScrollDownButton(buttonProps: ScrollDownButtonProps) {
+export default function ScrollDownButton(centerProps: ScrollDownButtonProps) {
   return (
     <MotionIconButton
       fontSize="xl"
       position="absolute"
       transform="translateX(-50%)"
+      cursor="pointer"
+      borderRadius="full"
       left="50%"
-      bottom="5%"
+      bottom="6%"
       variant="unstyled"
       aria-label="Scroll down"
-      icon={<FaAnglesDown />}
       initial={{ y: '0%' }}
-      animate={{ y: ['0%', '50%', '0%'] }}
+      animate={{ y: ['0%', '40%', '0%'] }}
       transition={{ duration: 0.8, repeat: Infinity, ease: 'linear' }}
-      {...buttonProps}
-    />
+      _hover={{
+        boxShadow: '0 0 20px 30px rgba(0,0,0,0.8)',
+        transition: 'box-shadow 0.3s ease-in-out',
+      }}
+      _after={{
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        top: '-20px', // 클릭 가능 영역 확장
+        right: '-20px',
+        bottom: '-20px',
+        left: '-20px',
+      }}
+      {...centerProps}
+    >
+      <FaAnglesDown />
+    </MotionIconButton>
   );
 }
