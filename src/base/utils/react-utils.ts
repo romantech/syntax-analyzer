eb7b69180c0d154d8f60d0bb91d19fa72f63c5aa
ColorFilter <johan@romantech.net>
2023-08-21 06:05:32 +0900

    🔧 chore(react-utils.ts): refactor lazyImport function to enhance React.lazy() with support for named imports 📝 docs(react-utils.ts): add JSDoc comments to lazyImport function to provide usage examples and references

--- src/base/utils/react-utils.ts
+++ src/base/utils/react-utils.ts
@@ -1,18 +1,28 @@
 import { ComponentType, lazy } from 'react';
 
-// named imports for React.lazy: https://github.com/facebook/react/issues/14603#issuecomment-726551598
-// Usage: const { Home } = lazyImport(() => import("./Home"), "Home");
-// @ts-expected-error TS complains about the return type of lazyImport, but it's correct
-export function lazyImport<
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  T extends ComponentType<any>,
-  I extends { [K2 in K]: T },
-  K extends keyof I,
->(factory: () => Promise<I>, name: K): I {
-  return Object.create({
-    [name]: lazy(() => factory().then((module) => ({ default: module[name] }))),
+/**
+ * A utility function that enhances React.lazy() by allowing named imports. This function
+ * provides a way to dynamically import individual components using their names.
+ *
+ * @example
+ * const { Home, About } = lazyImport(() => import('@/features/misc'));
+ *
+ * @see {https://github.com/facebook/react/issues/14603#issuecomment-736878172|React's discussion on named imports}
+ * @see {https://github.com/JLarky/react-lazily/blob/main/src/core/lazily.ts|react-lazily source code for a similar approach}
+ */
+export const lazyImport = <T extends NonNullable<unknown>, U extends keyof T>(
+  loader: (x?: string) => Promise<T>,
+) => {
+  return new Proxy({} as T, {
+    get: (_target, componentName: string) => {
+      return lazy(() =>
+        loader(componentName).then((module) => ({
+          default: module[componentName as U] as ComponentType<T>,
+        })),
+      );
+    },
   });
-}
+};
 
 export const getDisplayName = <T>(Component: ComponentType<T>) => {
   return Component.displayName || Component.name || 'Component';
