519bf8cbf97c219dd9500bd51e8c99cefc5efc52
ColorFilter <johan@romantech.net>
2023-08-23 05:29:15 +0900

    📝 docs(user-store.ts): add comment explaining the licensing changes in version 4.0 and the availability of free usage in version 3.x

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -1,6 +1,11 @@
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
 import { atom } from 'jotai';
 
+/**
+ * 4.0 버전부터 Business Source License 1.1이 적용돼서 배포 목적은 유료 결제 필요
+ * {@link https://github.com/fingerprintjs/fingerprintjs/blob/master/LICENSE 라이선스 정보}
+ * 3.x 버전은 배포 버전에서도 무료로 사용할 수 있지만 정확도는 떨어짐
+ * */
 export const fingerprintAtom = atom(async () => {
   const fp = await FingerprintJS.load({ monitoring: false });
 
