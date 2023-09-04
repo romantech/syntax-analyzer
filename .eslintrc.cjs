b312d5008fdb536c3282e6c4326629b6545b535a
ColorFilter <johan@romantech.net>
2023-09-04 16:40:44 +0900

    🔧 chore(.eslintrc.cjs): reorder eslint plugin extends to improve readability and grouping 🔧 chore(.eslintrc.cjs): add import plugin and typescript plugin extends to improve linting for imports and typescript code 🔧 chore(.eslintrc.cjs): add settings for react version detection and typescript resolver to improve linting accuracy 🔧 chore(.eslintrc.cjs): update rules for react-refresh plugin to allow constant exports and improve linting for react components 🔧 chore(.eslintrc.cjs): update rules for no-console and no-debugger to warn in production environment and off in other environments 🔧 chore(.eslintrc.cjs): update prettier/prettier rule to warn and enable endOfLine auto formatting 🔧 chore(.eslintrc.cjs): update no-param-reassign rule to allow reassigning properties

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,45 +6,68 @@ module.exports = {
     node: true, // Node.js 환경의 전역 변수 등록
   },
   extends: [
-    'plugin:react/jsx-runtime',
-    'plugin:@tanstack/eslint-plugin-query/recommended',
     'eslint:recommended',
-    'plugin:react/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', '@typescript-eslint'],
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': { typescript: true, node: true },
+  },
   rules: {
-    'react-refresh/only-export-components': 'warn',
-    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }, // 상수 내보내기 허용
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // Prettier 가 잡은 내용은 'warn' 으로 취급하도록 설정(ESLint - Prettier 충돌 방지) ▼
-    // { endOfLine: 'auto' }는 엔드라인 시퀀스 자동 변경 ▼
+    // Prettier 포맷 관련 내용은 'warn' 으로 취급(ESLint/Prettier 충돌 방지) ▼
+    // { endOfLine: 'auto' } 엔드라인 시퀀스 자동 변경 ▼
     'prettier/prettier': ['warn', { endOfLine: 'auto' }],
     'no-param-reassign': ['error', { props: false }],
-    'prefer-const': 1,
-    'no-plusplus': 0,
-    'vars-on-top': 0,
-    'no-underscore-dangle': 0, // var _foo;
-    'comma-dangle': 0,
-    'func-names': 0, // setTimeout(function () {}, 0);
-    'prefer-arrow-callback': 0, // setTimeout(function () {}, 0);
-    'prefer-template': 0,
-    'no-nested-ternary': 0,
-    'max-classes-per-file': 0,
-    'no-restricted-syntax': [0, 'ForOfStatement'],
-    'consistent-return': 1,
-    'no-unused-vars': 0,
-    'no-unused-expressions': 1,
-    'react/prop-types': 0,
-    'react-hooks/exhaustive-deps': [
+    'react/react-in-jsx-scope': 'off',
+    'prefer-const': 'warn',
+    'no-plusplus': 'off',
+    'vars-on-top': 'off',
+    'no-underscore-dangle': 'off', // var _foo;
+    'comma-dangle': 'off',
+    'func-names': 'off', // setTimeout(function () {}, 0);
+    'prefer-arrow-callback': 'off', // setTimeout(function () {}, 0);
+    'prefer-template': 'off',
+    'no-nested-ternary': 'off',
+    'max-classes-per-file': 'off',
+    'no-restricted-syntax': ['off', 'ForOfStatement'],
+    'consistent-return': 'warn',
+    'react/prop-types': 'off',
+    'no-unused-expressions': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+
+    /** import 정렬 관련 설정 */
+    'import/no-unresolved': 'error',
+    'import/order': [
       'warn',
-      { additionalHooks: '(useRecoilCallback|useRecoilTransaction_UNSTABLE)' },
+      {
+        groups: [
+          'builtin', // Built-in imports go first
+          'external', // External imports
+          'internal', // Absolute imports
+          ['sibling', 'parent'], // Relative imports from siblings and parents can mix
+          'index', // index imports
+          'unknown', // unknown
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
     ],
-    '@typescript-eslint/no-unused-vars': 1,
   },
 };
