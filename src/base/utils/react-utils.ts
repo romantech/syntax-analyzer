81147b2a3fe331d5a9bd854c11a7790989113a40
ColorFilter <johan@romantech.net>
2023-09-14 19:30:54 +0900

    🔧 fix(react-utils.ts): refactor lazyImport function to improve readability and maintainability
    📝 chore(react-utils.ts): add type aliases for better code documentation and readability
    
    💡 refactor(react-utils.ts): simplify lazyImport function by removing unnecessary parameter and using async/await syntax

--- src/base/utils/react-utils.ts
+++ src/base/utils/react-utils.ts
@@ -1,5 +1,9 @@
 import { ComponentType, lazy } from 'react';
 
+type ComponentName = string;
+type Loader<T> = () => Promise<T>;
+type LazyImportType = Record<ComponentName, unknown>;
+
 /**
  * A utility function that enhances React.lazy() by allowing named imports. This function
  * provides a way to dynamically import individual components using their names.
@@ -10,16 +14,13 @@ import { ComponentType, lazy } from 'react';
  * @see {https://github.com/facebook/react/issues/14603#issuecomment-736878172 React's discussion on named imports}
  * @see {https://github.com/JLarky/react-lazily/blob/main/src/core/lazily.ts react-lazily source code for a similar approach}
  */
-export const lazyImport = <T extends NonNullable<unknown>, U extends keyof T>(
-  loader: (x?: string) => Promise<T>,
-) => {
+export const lazyImport = <T extends LazyImportType>(loader: Loader<T>) => {
   return new Proxy({} as T, {
-    get: (_target, componentName: string) => {
-      return lazy(() =>
-        loader(componentName).then((module) => ({
-          default: module[componentName as U] as ComponentType<T>,
-        })),
-      );
+    get: (_target, name: ComponentName) => {
+      return lazy(async () => {
+        const module = await loader();
+        return { default: module[name] as ComponentType<T> };
+      });
     },
   });
 };
