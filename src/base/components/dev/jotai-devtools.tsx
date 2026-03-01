bca9f4d272ac2629a72499944e69e0bc96184eec
ColorFilter <johan@romantech.net>
2026-03-01 21:19:30 +0900

    🐛 Jotai DevTools 상태 안나오는 문제 해결 (#73)

--- src/base/components/dev/jotai-devtools.tsx
+++ src/base/components/dev/jotai-devtools.tsx
@@ -1,19 +1,8 @@
-import { lazy, Suspense } from 'react';
+import { DevTools, DevToolsProps } from 'jotai-devtools';
 
-import { DevToolsProps } from 'jotai-devtools';
+if (import.meta.env.DEV) import('jotai-devtools/styles.css');
 
-const LazyDevTools = lazy(() =>
-  Promise.all([
-    import('jotai-devtools'),
-    import('jotai-devtools/styles.css'),
-  ]).then(([m]) => ({ default: m.DevTools })),
-);
-
-export function JotaiDevTools(props: DevToolsProps) {
+export function JotaiDevTools({ theme = 'dark', ...props }: DevToolsProps) {
   if (!import.meta.env.DEV) return null;
-  return (
-    <Suspense fallback={null}>
-      <LazyDevTools {...props} />
-    </Suspense>
-  );
+  return <DevTools theme={theme} {...props} />;
 }
